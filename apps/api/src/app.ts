c25c5220121d8e179080cc9a2713b3dfae508f98
itizawa <itizawa.pen@gmail.com>
2024-11-09 15:51:21 +0900

    fix

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,12 +1,9 @@
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { json, urlencoded } from 'body-parser';
-import connectPgSimple from 'connect-pg-simple';
 import cors from 'cors';
 import express from 'express';
 import session from 'express-session';
 import morgan from 'morgan';
-import passport from 'passport';
-import { Pool } from 'pg';
 import { postRouter } from './controllers/postRouter';
 import { questionRouter } from './controllers/questionRouter';
 import { createContext, router } from './trpc';
@@ -18,15 +15,11 @@ export const appRouter = router({
 // export type definition of API
 export type AppRouter = typeof appRouter;
 
-const PgSession = connectPgSimple(session);
-
 const app = express();
 
 if (!process.env.SESSION_SECRET || !process.env.DATABASE_URL) {
   throw new Error('Please set env variables SESSION_SECRET and DATABASE_URL');
 }
-
-const url = new URL(process.env.DATABASE_URL);
 
 app.use(
   session({
@@ -41,22 +34,8 @@ app.use(
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
     },
-    store: new PgSession({
-      tableName: 'sessions',
-      pool: new Pool({
-        user: url.username,
-        password: url.password,
-        host: url.hostname,
-        database: url.pathname.slice(1),
-        port: Number(url.port),
-      }),
-    }),
   }),
 );
-
-// Passportの初期化
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use(express.json());
 app.disable('x-powered-by');
