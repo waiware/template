829ef8a76393d679b31a656f9c6a497eb160117d
itizawa <itizawa.pen@gmail.com>
2024-11-23 21:19:29 +0900

    Fix: answer wお mock に追加

--- packages/types/src/mocks/createMockQuestion.ts
+++ packages/types/src/mocks/createMockQuestion.ts
@@ -5,6 +5,7 @@ export const createMockQuestion = (init: Partial<Question>): Question => {
     id: init.id || 'question1',
     title: init.title || 'title',
     body: init.body || 'body',
+    answer: init.answer || 'answer',
     createdAt: init.createdAt || new Date(),
     updatedAt: init.updatedAt || new Date(),
   };
