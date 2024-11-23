997b1233c6fd03ca2003b77f0e86e3b70a459c19
itizawa <itizawa.pen@gmail.com>
2024-11-23 10:41:27 +0900

    Fix: test

--- packages/api/jest.config.js
+++ packages/api/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  testMatch: ['<rootDir>/src/**/+(*.)+(spec|test).+(ts|js)'],
   preset: 'ts-jest',
   testEnvironment: '@quramy/jest-prisma/environment', // ここを追記
   moduleNameMapper: {
