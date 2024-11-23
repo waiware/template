ef4d8ff5130fb78dcabf7fa23219511671bd54c2
itizawa <itizawa.pen@gmail.com>
2024-11-23 13:32:57 +0900

    Clean: fixtures を実装してテストを改善した

--- packages/api/src/usecases/CreatePostUseCase/CreatePostUseCase.spec.ts
+++ packages/api/src/usecases/CreatePostUseCase/CreatePostUseCase.spec.ts
@@ -1,20 +1,25 @@
 import { createMockQuestion, createMockUser } from '@repo/types';
 import { prismaClient } from '../../libs/PrismaClientSingleton';
 import { cleanUpAllTableForTest } from '../../test-utils/cleanUpAllTableForTest';
+
+import { createQuestionsFixtures } from '../../test-utils/fixtures/createQuestionsFixtures';
+import { createUsersFixtures } from '../../test-utils/fixtures/createUsersFixtures';
+
 import { CreatePostUseCase } from './CreatePostUseCase';
 
 const createPostUseCase = new CreatePostUseCase(prismaClient);
 
 describe('CreatePostUseCase', () => {
   beforeEach(async () => {
-    await prismaClient.user.create({ data: createMockUser({ id: 'user1' }) });
-    await prismaClient.question.create({ data: createMockQuestion({ id: 'question1' }) });
+    await createUsersFixtures([createMockUser({ id: 'user1' })]);
+    await createQuestionsFixtures([createMockQuestion({ id: 'question1' })]);
   });
   afterEach(async () => {
     await cleanUpAllTableForTest();
   });
   describe('正常系', () => {
     it('質問を生成できる', async () => {
+      // ACT
       await createPostUseCase.execute({
         body: 'Example',
         userId: 'user1',
