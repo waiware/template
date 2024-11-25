1b93716959ce9be7b60f51414633821a11bbb59d
itizawa <itizawa.pen@gmail.com>
2024-11-25 20:46:57 +0900

    Change: 回答結果に関するスタイルの修正

--- apps/web/src/components/models/correctResult/CorrectResultInformation/CorrectResultInformation.tsx
+++ apps/web/src/components/models/correctResult/CorrectResultInformation/CorrectResultInformation.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const CorrectResultInformation: React.FC<Props> = ({ correctResult, questionAnswer }) => {
   return (
-    <Paper sx={{ p: 2 }} square elevation={0}>
+    <Paper sx={{ p: 2, borderRadius: 2 }} square elevation={0}>
       <Box display='flex' justifyContent='center' alignItems='center'>
         <CheckCircle sx={{ height: 24, width: 24, color: props => props.palette.success.main }} />
         <Typography variant='h6' align='center' sx={{ fontWeight: 'bold', alignItems: 'center' }}>
@@ -17,9 +17,34 @@ export const CorrectResultInformation: React.FC<Props> = ({ correctResult, quest
         </Typography>
       </Box>
       <Stack rowGap={1}>
-        <Typography variant='body2'>質問数：{correctResult.numberOfPosts}</Typography>
-        <Typography variant='body2'>回答にかかった時間：{correctResult.elapsedSeconds}秒</Typography>
-        <Typography variant='body2'>正解：{questionAnswer}</Typography>
+        <Stack rowGap={0.5}>
+          <Typography variant='body2' sx={{ color: props => props.palette.text.secondary }}>
+            質問数
+          </Typography>
+          <Typography variant='body2'>
+            <Typography component='span' sx={{ fontWeight: 'bold' }}>
+              {correctResult.numberOfPosts}
+            </Typography>{' '}
+            回
+          </Typography>
+        </Stack>
+        <Stack rowGap={0.5}>
+          <Typography variant='body2' sx={{ color: props => props.palette.text.secondary }}>
+            回答にかかった時間
+          </Typography>
+          <Typography variant='body2'>
+            <Typography component='span' sx={{ fontWeight: 'bold' }}>
+              {correctResult.elapsedSeconds}
+            </Typography>{' '}
+            秒
+          </Typography>
+        </Stack>
+        <Stack rowGap={0.5}>
+          <Typography variant='body2' sx={{ color: props => props.palette.text.secondary }}>
+            真相
+          </Typography>
+          <Typography variant='body2'>{questionAnswer}</Typography>
+        </Stack>
       </Stack>
     </Paper>
   );
