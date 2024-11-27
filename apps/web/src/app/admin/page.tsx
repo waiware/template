fa719b5f227e10f06540bdbddc26274fe365ed90
itizawa <itizawa.pen@gmail.com>
2024-11-28 08:51:29 +0900

    Change: 管理画面で問題一覧を表示するようにした

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -1,9 +1,16 @@
-import { Stack } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import { listQuestions } from '~/actions/question';
+import { QuestionListForAdmin } from '~/components/models/question/QuestionListForAdmin';
 
 export default async function Page() {
+  const questions = await listQuestions();
+
   return (
-    <Stack px={1}>
-      <Stack rowGap={5}>{/*  */}</Stack>
+    <Stack pt={3} px={1} rowGap={3}>
+      <Typography variant='h5' textAlign='center' sx={{ fontWeight: 'bold' }}>
+        管理画面
+      </Typography>
+      <QuestionListForAdmin questions={questions} />
     </Stack>
   );
 }
