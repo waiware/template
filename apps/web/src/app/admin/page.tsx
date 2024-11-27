58bd36bcbfe154b572981e229203720b44500060
itizawa <itizawa.pen@gmail.com>
2024-11-28 08:45:39 +0900

    Change: `/admin` にアクセスして管理画面にアクセスする

---
+++ apps/web/src/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import { Stack } from '@mui/material';
+
+export default async function Page() {
+  return (
+    <Stack px={1}>
+      <Stack rowGap={5}>{/*  */}</Stack>
+    </Stack>
+  );
+}
---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,35 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get('Authorization');
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1];
+    // atob is deprecated but Buffer.from is not available in Next.js edge.
+    const [user, password] = atob(authValue).split(':');
+
+    if (user === 'psychopath' && password === 'psychopath1234') {
+      return NextResponse.next();
+    }
+
+    return NextResponse.json(
+      { error: 'Invalid credentials' },
+      {
+        headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+        status: 401,
+      },
+    );
+  }
+
+  return NextResponse.json(
+    { error: 'Please enter credentials' },
+    {
+      headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' },
+      status: 401,
+    },
+  );
+}
