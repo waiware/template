16b51bbfb0503385ec798f7ecae05d8ea231a83b
itizawa <itizawa.pen@gmail.com>
2024-11-30 20:10:56 +0900

    Fix: style

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -8,7 +8,7 @@ export default async function Page() {
   const questionsForAdmin = await listQuestionsForAdmin();
 
   return (
-    <Stack pt={3} px={1} rowGap={3}>
+    <Stack py={3} px={1} rowGap={3}>
       <Box display='flex' justifyContent='space-between' alignItems='center'>
         <Typography variant='h5' textAlign='center' sx={{ fontWeight: 'bold' }}>
           管理画面
